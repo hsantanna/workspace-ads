668d2216865571c5132ee2876beb0d3b14a55d3e
Henique Sant'Anna <hsantanna@gmail.com>
2016-10-19 20:14:35 -0200

    seleciona país com selectOneMenu

--- hello/src/com/prog4/jsf/hello/Estudante.java
+++ hello/src/com/prog4/jsf/hello/Estudante.java
@@ -1,5 +1,8 @@
 package com.prog4.jsf.hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
@@ -7,9 +10,25 @@ public class Estudante {
 
 	private String firstName;
 	private String lastName;
+	private String pais;
+	
+	List<String> opcoesPais;
+	
+	
 	
 	// construtor
 	public Estudante() {
+		
+		// preenche lista de países
+		opcoesPais = new ArrayList<>();
+			
+		opcoesPais.add("Brasil");
+		opcoesPais.add("France");
+		opcoesPais.add("Germany");
+		opcoesPais.add("India");
+		opcoesPais.add("Turkey");
+		opcoesPais.add("United Kingdom");
+		opcoesPais.add("United States");
 		
 	}
 	
@@ -34,6 +53,18 @@ public class Estudante {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public List<String> getOpcoesPais() {
+		return opcoesPais;
 	}
 
 	
